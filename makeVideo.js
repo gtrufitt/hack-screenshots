a354f32779de4917736295f8f25e1adc3ccc804b
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-09 15:44:04 +0000

    Add functionality to create video between a date range

--- makeVideo.js
+++ makeVideo.js
@@ -1,21 +1,26 @@
 const videoshow = require('videoshow');
+const fs = require('fs');
+const fsp = require('fs-promise');
+const junk = require('junk');
 
-function getImages(site) {
-    return [
-        `./public/images/screenshots/${site}/1488712488559.png`,
-        `./public/images/screenshots/${site}/1488712860997.png`,
-        `./public/images/screenshots/${site}/1488713400848.png`,
-        `./public/images/screenshots/${site}/1488715740005.png`,
-        `./public/images/screenshots/${site}/1488712488559.png`,
-        `./public/images/screenshots/${site}/1488712860997.png`,
-        `./public/images/screenshots/${site}/1488713400848.png`,
-        `./public/images/screenshots/${site}/1488715740005.png`
-    ];
+function getImageFromDateRange(folderPath, {dateFrom, dateTo}) {
+    return file => fs.statSync(folderPath + file, (err, stats) => {
+        const milliBirthTime = new Date(stats.birthtime).getTime();
+        return milliBirthTime > dateFrom && milliBirthTime < dateTo;
+    });
+}
+
+function getImages(site, selectionOptions) {
+    const folderPath = `./public/images/screenshots/${site}/`;
+    return fsp.readdir(folderPath).then(files => files
+            .filter(getImageFromDateRange(folderPath, selectionOptions))
+            .filter(junk.not)
+            .map(file => folderPath + file));
 }
 
 const defVideoOptions = {
     fps: 25,
-    loop: 0.5, // seconds
+    loop: 0.2, // seconds
     transition: false,
     videoBitrate: 1024,
     videoCodec: 'libx264',
@@ -24,13 +29,16 @@ const defVideoOptions = {
     pixelFormat: 'yuv420p'
 };
 
-function makeVideo(site, options) {
-    const videoOptions = Object.assign(defVideoOptions, options);
-    return new Promise((resolve, reject) => {
-        videoshow(getImages(site), videoOptions)
-            .save('./public/videos/test.mp4')
+function makeVideo(site, selectionOptions, videoOptions) {
+    const mergedVideoOptions = Object.assign(defVideoOptions, videoOptions);
+    return new Promise((resolve, reject) => getImages(
+        site,
+        selectionOptions
+    ).then(imageArr => {
+        videoshow(imageArr, mergedVideoOptions)
+            .save(`./public/videos/${site}-${Date.now()}.mp4`)
             .on('start', command => {
-                console.log('ffmpeg process started:', command);
+                console.log('ffmpeg process started');
             })
             .on('error', (err, stdout, stderr) => {
                 console.error('Error:', err);
@@ -39,9 +47,11 @@ function makeVideo(site, options) {
             })
             .on('end', output => {
                 console.error('Video created in:', output);
-                resolve('Video created');
+                return resolve(
+                    `Video created: <a href="videos/${site}-${Date.now()}.mp4>Download</a>`
+                );
             });
-    });
+    }));
 }
 
 module.exports = makeVideo;
--- routes/makeVideo.js
+++ routes/makeVideo.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 /* Take a screenshot. */
 router.post('/', (req, res) => {
-    makeVideo('thedailyfail')
+    makeVideo('theguardian', {
+        dateFrom: new Date('2017-03-09T10:51:15.000Z').getTime(),
+        dateTo: new Date('2017-03-09T12:51:15.000Z').getTime()
+    })
         .then(response => {
             res.send(response);
         })
